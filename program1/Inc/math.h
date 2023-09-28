c2792fede40b0c21594ca281ebee4a7417e1ece5
sethupathy <125538158+SETHUPATHY12@users.noreply.github.com>
2023-09-28 01:52:42 -0700

    Add files via upload

---
+++ program1/Inc/math.h
@@ -0,0 +1,6 @@
+#ifndef MATH_H_
+#define MATH_H_
+int math_add(int a,int b);
+int math_mul(int a,int b);
+int math_div(int a,int b);
+#endif
---
+++ program1/Src/math.c
@@ -0,0 +1,16 @@
+int math_add(int a,int b);
+int math_mul(int a,int b);
+int math_div(int a,int b);
+
+
+
+int math_add(int a,int b)
+{return a+b;}
+int math_mul(int a,int b)
+{
+	return a*b;
+	}
+int math_div(int a,int b)
+{
+	return a/b;
+	}
